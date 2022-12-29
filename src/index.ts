208d9b57f5a5f1280ab73c6ea41231ca2fac6ec0
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-12-29 13:33:32 +0700

    Setup Husky

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ const app: Application = express()
 const port: Number = 4000
 
 app.use('/health', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({
-        status: '200',
-        message: 'Server is Normal Running'
-    })
+  res.status(200).send({
+    status: '200',
+    message: 'Server is Normal Running'
+  })
 })
 
 app.listen(port, () => console.log(`Server is Listening ${port}`))
