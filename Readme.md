d752f524b8e72628e320266702b765c505d45cfb
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-12-29 13:10:21 +0700

    Setup Prettier

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@
 3. Run ``npm i nodemon``
 4. Run ``npm i express``
 5. Run ``npm i -D @types/express``
+6. Run ``npx eslint --init``
+7. Run ``npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-standard eslint-plugin-import eslint-plugin-node eslint-plugin-promise eslint-plugin-standard``
+8. Run ``npm i --save-dev --save-exact prettier``
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from 'express'
 
-const app: Application = express();
-const port: Number = 4000;
+const app: Application = express()
+const port: Number = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
     res.status(200).send({
-        status: "200",
-        message: "Server is Normal Running"
+        status: '200',
+        message: 'Server is Normal Running'
     })
 })
 
-app.listen(port, () => console.log(`Server is Listening ${port}`));
+app.listen(port, () => console.log(`Server is Listening ${port}`))
