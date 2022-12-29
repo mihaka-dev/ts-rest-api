a102d365d4d642fd5fc6cf615376a2c7c4135071
Arif Sulaiman <arif.sulaiman720@gmail.com>
2022-12-29 11:11:13 +0700

    Setup Express

--- Readme.md
+++ Readme.md
@@ -1,3 +1,5 @@
 1. Run ``npm i -D typescript ts-node``
 2. Run ``npx tsc --init``
 3. Run ``npm i nodemon``
+4. Run ``npm i express``
+5. Run ``npm i -D @types/express``
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("test");
+import express, { Application, NextFunction, Request, Response } from "express";
+
+const app: Application = express();
+const port: Number = 4000;
+
+app.use("/", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({
+        data: "Hello World"
+    })
+})
+
+app.listen(port, () => console.log(`Server is Listening ${port}`));
